c593e1cc71cc0ab15cd45f074cbdfcb6eebdc3c9
TaoKY <taoky1999@gmail.com>
2016-07-01 12:02:52 +0800

    Employing M$'s Sample.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <Windows.h>
+#include "Wallpaper.h" // From M$
 using namespace std;
+
+// Using M$'s sample to solve it.
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	
 	return 0;
 }
