6ff029cf12c6cc96ab6a5238c6bd3c9d796c465c
TaoKY <taoky1999@gmail.com>
2016-07-10 18:11:08 +0800

    Basic function finished. Testing for WinXP.

--- main.cpp
+++ main.cpp
@@ -11,9 +11,10 @@ using namespace System::Drawing::Imaging;
 // Necessary to install VC++ 2015 Library and .NET 4.0
 
 const int MAX = 50000;
+BOOL isnotXP = SupportJpgAsWallpaper();
 HANDLE LogFile;
 
-#pragma unmanaged
+// #pragma unmanaged
 // This pragma is used to set breakpoint in unmanaged (Not CLR) C++ code.
 
 void WriteLog(LPSTR lpWord, HANDLE hFile)
@@ -32,6 +33,9 @@ void WriteTimeToLog()
 {
 	SYSTEMTIME NowTime;
 	TCHAR      ReturnTimeString[MAX];
+	
+	WriteLog("Test", LogFile);
+	WriteLog(L"/Test/", LogFile);
 	GetLocalTime(&NowTime);
 	wsprintf(ReturnTimeString, L"%d Äê %d ÔÂ %d ÈÕ %d:%d:%d:%d ", NowTime.wYear, NowTime.wMonth, NowTime.wDay, NowTime.wHour, NowTime.wMinute, NowTime.wSecond, NowTime.wMilliseconds);
 	WriteLog(ReturnTimeString, LogFile);
@@ -39,7 +43,7 @@ void WriteTimeToLog()
 
 void CopyPic(LPTSTR FromPath, LPTSTR ToPath)
 {
-	LPTSTR List[] = {L"*.jpg", L"*.png", L"*.bmp"};
+	LPTSTR List[] = {L"*.jpg", L"*.png", L"*.bmp"}; // Warning: DO NOT SUPPORT *.jpeg
 	lstrcat(FromPath, L"\\");
 	lstrcat(ToPath, L"\\");
 	TCHAR tFromPath[MAX];
@@ -63,7 +67,7 @@ void CopyPic(LPTSTR FromPath, LPTSTR ToPath)
 	{
 		do
 		{
-			if (toDelPicData.cFileName != L"Flag")
+			if (lstrcmp(toDelPicData.cFileName, L"Flag"))
 			{
 				TCHAR DelFileName[MAX];
 				lstrcpy(DelFileName, ToPath);
@@ -110,16 +114,23 @@ void CopyPic(LPTSTR FromPath, LPTSTR ToPath)
 				WriteLog(ToFilePath, LogFile);
 				WriteLog("\r\n", LogFile);
 				CopyFile(FromFilePath, ToFilePath, FALSE);
+				if (!isnotXP && wcsstr(_wcslwr(hFileData.cFileName), L".jpg") || wcsstr(_wcslwr(hFileData.cFileName), L".png"))
+				{
+					WriteLog("Converting", LogFile);
+					System::String ^CLRFilePath = gcnew System::String(ToFilePath);
+					Image^ image = Image::FromFile(CLRFilePath);
+					image->Save(CLRFilePath + ".bmp", ImageFormat::Bmp);
+				}
 			}
 		} while (FindNextFile(hFile, &hFileData));
 
 	}
 }
 
-void CALLBACK TimerProc(HWND hWnd, UINT nMsg, UINT nTimerid, DWORD dwTime)
+/* void CALLBACK TimerProc(HWND hWnd, UINT nMsg, UINT nTimerid, DWORD dwTime)
 {
-	WriteLog("Hahaha", LogFile);
-}
+	
+} */
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -150,7 +161,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 	
 	// Reading settings.ini
-	int Time;
+	unsigned int Time;
 	TCHAR LogFilePath[MAX], PicFileDic[MAX], Style[MAX];
 	GetPrivateProfileString(TEXT("Settings"),
 							TEXT("Log"),
@@ -175,7 +186,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 								-1, 
 								TEXT("./settings.ini"));
 	// Juding whether these strings are vaild.
-	if (Time <= 0 || GetFileAttributes(PicFileDic) != FILE_ATTRIBUTE_DIRECTORY ||
+	if (Time * 1000 <= 0 || GetFileAttributes(PicFileDic) != FILE_ATTRIBUTE_DIRECTORY ||
 		(lstrcmp(Style, L"Center") && lstrcmp(Style, L"Tile") && lstrcmp(Style, L"Stretch")))
 	{
 		MessageBox(NULL, TEXT("Please modify the settings.ini correctly. This program will exit now."), TEXT("Warning"), MB_OK);
@@ -258,7 +269,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 
 	// Setting Timer and begin to change wallpaper
-	SetTimer(NULL, 0, 1000, &TimerProc);
+	/* SetTimer(NULL, 0, Time * 1000, &TimerProc);
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
@@ -266,6 +277,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		{
 			DispatchMessage(&msg);
 		}
+	} */
+	WriteLog("Preparing to set wallpapers.\r\n", LogFile);
+	while (1)
+	{
+		TCHAR SetPicPath[MAX];
+		lstrcpy(SetPicPath, TMPDic);
+		lstrcat(SetPicPath, L"\\*");
+		WIN32_FIND_DATA PicData;
+		HANDLE PicFile = FindFirstFile(SetPicPath, &PicData);
+		do
+		{
+			if (lstrcmp(PicData.cFileName, L"Flag") && 
+				(wcsstr(_wcslwr(PicData.cFileName), L".jpg")) || wcsstr(_wcslwr(PicData.cFileName), L".bmp") || wcsstr(_wcslwr(PicData.cFileName), L".png"))
+			{
+				if (!isnotXP && (wcsstr(_wcslwr(PicData.cFileName), L".jpg") || wcsstr(_wcslwr(PicData.cFileName), L".png")))
+					continue;
+				TCHAR Path[MAX];
+				wsprintf(Path, L"%s\\%s", TMPDic, PicData.cFileName);
+				WriteLog(Path, LogFile);
+				WriteLog("\r\n", LogFile);
+				SetDesktopWallpaper(Path, WStyle);
+				Sleep(Time * 1000);
+			}
+		} while (FindNextFile(PicFile, &PicData));
+		
 	}
 	return 0;
 }
